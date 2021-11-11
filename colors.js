feb38ebd478ffa3c987389f20a17e5961374d5f2
Alejo Dieguez <alejodieguez2012@gmail.com>
2021-11-11 15:41:58 -0300

    changed repo directory

--- colors.js
+++
@@ -1,7 +0,0 @@
-//COLOR PALETTE
-    //PRIMARY COLOR
-        const primaryColor = "#e2d4c7"
-    //SECONDARY COLOR
-        const secondaryColor = "#bba69c"
-    //OPTIONS COLORS
-        const otherColor = "#9d9790" 
