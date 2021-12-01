b50b85b7f9ded832def95adf53d1ce8602f1e2ec
Alejo Dieguez <alejodieguez2012@gmail.com>
2021-11-30 23:45:52 -0300

    applied json file into products section

--- javascript/products.js
+++ javascript/products.js
@@ -8,39 +8,49 @@ let productos = [
   { id: 2, producto: "Combo Velas X2", precio: "$" + 250, stock: 2 },
 ];
 
+
 let carrito = [];
 
 productos.forEach((producto) => {
   const product = document.createElement("div");
   product.className = "product-container";
-
+  
   product.innerHTML = `
-      <div class="product">
-      <img src="../assets/300x300.png" alt="Producto relacionado a velas, decoracion, etc">
-      <p>${producto.producto}</p>
-      <p>${producto.precio}</p>
-      <button id="cart-${producto.id}" type="button">Agregar a carrito</button>
-      </div>`;
-
+  <div class="product">
+  <img src="../assets/300x300.png" alt="Producto relacionado a velas, decoracion, etc">
+  <p>${producto.producto}</p>
+  <p>${producto.precio}</p>
+  <button id="cart-${producto.id}" class="btn-cart" type="button">Agregar a carrito</button>
+  </div>`;
+  
   $("#productsSection").append(product);
-
+  
   $(".product-container").fadeOut(3, function () {
     $(".product-container").fadeIn(800);
   });
-
+  
   $(`#cart-${producto.id}`).click(function () {
     carrito.push(producto);
     producto.stock = producto.stock - 1;
     const product = document.createElement("div");
     product.className = "product-container";
-
+    
     product.innerHTML = `
-          <div class="product">
-          <img src="../assets/300x300.png" alt="Producto relacionado a velas, decoracion, etc">
-          <h5>${producto.producto}</h5>
-          <p>${producto.precio}</p>
-          </div>
-          </div>`;
+    <div class="product">
+    <img src="../assets/300x300.png" alt="Producto relacionado a velas, decoracion, etc">
+    <h5>${producto.producto}</h5>
+    <p>${producto.precio}</p>
+    </div>
+    </div>`;
     $("#cart-section").append(product);
   });
 });
+ const GETURL = "../json/products.json";
+ $(".btn-cart").click(() => {
+   $.getJSON(GETURL, function(respuesta, estado) {
+     if(estado == "success") {
+       let misDatos = respuesta;
+       console.log(misDatos[0].producto);
+     }
+   })
+ })
