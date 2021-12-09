9a9cf74b07a1dc15f02a84871eb698361ce9dcf3
Alejo Dieguez <alejodieguez2012@gmail.com>
2021-12-09 16:16:25 -0300

    Deleted delete button from cart, added finish purchase section

--- javascript/cart.js
+++ javascript/cart.js
@@ -7,24 +7,14 @@ carrito.forEach((producto) => {
     <div class="product">
     <img src="${producto.image}" alt="Producto relacionado a velas, decoracion, etc">
     <p>${producto.nombre}</p>
-    <p>${producto.precio}</p>
-    <p>${producto.cantidad}</p>
-    <button id="delete-${producto.id}" class="delete-cart" type="button">Eliminar</button>
+    <p>$${producto.precio}</p>
+    <p>Cantidad:${producto.cantidad}</p>
     </div>`;
 
   $("#productsSection").append(product);
 
   $(".product-container").fadeOut(3, function () {
     $(".product-container").fadeIn(800);
-  });
-  $(`#delete-${producto.id}`).click(function () {
-    if ((producto.cantidad == 1)) {
-      carrito.pop(producto);
-    } else {
-      producto.cantidad = producto.cantidad - 1;
-    }
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    location.reload();
   });
 });
 if (carrito == 0) {
